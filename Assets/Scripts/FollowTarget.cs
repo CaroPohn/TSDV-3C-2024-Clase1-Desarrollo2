325563fe7e4814b79a1a50a49b6dec4d7aa67883
CaroPohn <carolinapohn@gmail.com>
2024-03-20 10:50:04 -0300

    Add camera rotation

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -6,9 +6,22 @@ public class FollowTarget : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset = new Vector3(0, 1.5f, -5);
+    public float rotationSpeed = 5;
+    public float followSpeed = 5;
 
     void LateUpdate()
     {
-        transform.position = target.position + offset;
+        //Position
+        var offsetBasedOnTarget = target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, offsetBasedOnTarget, Time.deltaTime * followSpeed);
+
+        //Rotation
+        var desiredRotation = target.rotation * Quaternion.Euler(transform.rotation.eulerAngles.x, 0, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+
+        //Lerp = Linear Interpolation. Es una linea recta, sirve mejor para posiciones.
+
+        //Slerp = Interpola entre rotaciones, busca puntos dentro de una circunferencia para una rotacion mas "correcta". Es mejor para
+        //        rotaciones.
     }
 }
