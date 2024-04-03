39fe277890a3bd345063d3fc28840a141cf78207
CaroPohn <carolinapohn@gmail.com>
2024-04-03 10:48:09 -0300

    Corregir camara y add force principio

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterMovement movement;
+
+    void Update()
+    {
+        if(animator && movement)
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,15 +9,34 @@ public class CharacterMovement : MonoBehaviour
     public float speed = 1;
     public float rotationSpeed = 1;
     private Vector3 _desiredDirection;
+    [SerializeField] private Rigidbody rBody;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
 
+    private void OnValidate()
+    {
+        if(rBody == null) 
+        {
+            rBody = GetComponent<Rigidbody>();
+        }
+    }
+
     public void Move(Vector3 direction)
     {
-        _desiredDirection = direction;
+        Transform localTransform = transform;
+        
+        var camera = Camera.main;
+        if (camera != null)
+        {
+            localTransform = camera.transform;
+        }
+        var characterLocalDirection = transform.TransformDirection(direction);
+        characterLocalDirection.y = 0;
+        _desiredDirection = characterLocalDirection;
+
         animator.SetBool("isRunning", true);
 
         if (direction == Vector3.zero) 
@@ -28,9 +47,21 @@ public class CharacterMovement : MonoBehaviour
 
     private void Update()
     {
-        transform.position += _desiredDirection * speed * Time.deltaTime;
+        //transform.position += _desiredDirection * speed * Time.deltaTime;
 
         float angle = Vector3.SignedAngle(transform.forward, _desiredDirection, transform.up); //Necesitamos el angulo mas corto para rotar, por eso usamos SignedAngle
         transform.Rotate(transform.up, angle * Time.deltaTime * rotationSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        var currentVelocity = rBody.velocity;
+        currentVelocity.y = 0;
+
+        var currentSpeed = rBody.velocity.magnitude;
+        if (currentSpeed < speed)
+        {
+            rBody.AddForce(_desiredDirection * speed, ForceMode.Force);
+        }
     }
 }
